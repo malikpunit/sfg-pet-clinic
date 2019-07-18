bcded72bbc1f601b7d0434d2fb897763bc2a1aed
Punit Malik <punit.malik@niceactimize.com>
2019-07-18 21:26:03 +0530

    Implementing Interfaces for Services. Closes #5

---
+++ pet-clinic-data/src/main/java/malik/punit/sfgpetclinic/services/PetService.java
@@ -0,0 +1,13 @@
+package malik.punit.sfgpetclinic.services;
+
+import malik.punit.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/malik/punit/sfgpetclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package malik.punit.sfgpetclinic.services;
+
+import malik.punit.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
