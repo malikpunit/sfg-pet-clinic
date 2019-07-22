c3fe0ce590e249e88570fad3e32db736122b71e0
Punit Malik <punit.malik@niceactimize.com>
2019-07-22 14:05:41 +0530

    refactoring to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/malik/punit/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/malik/punit/sfgpetclinic/services/PetService.java
@@ -2,12 +2,5 @@ package malik.punit.sfgpetclinic.services;
 
 import malik.punit.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
