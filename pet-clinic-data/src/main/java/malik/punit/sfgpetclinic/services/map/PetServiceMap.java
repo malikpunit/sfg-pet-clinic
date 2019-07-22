7046e8074cb2c5f39bc6440fb95e7bb842e40c0a
Punit Malik <punit.malik@niceactimize.com>
2019-07-22 14:42:25 +0530

    Impl Map Based Services. Closes #6

---
+++ pet-clinic-data/src/main/java/malik/punit/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package malik.punit.sfgpetclinic.services.map;
+
+import malik.punit.sfgpetclinic.model.Pet;
+import malik.punit.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+
+        return super.findById(id);
+    }
+}
